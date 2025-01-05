924f78f89723aa8c8ea6e4ac6ec2af444bc33963
เคเบิล <cbkcontactmail@gmail.com>
2025-01-05 19:20:30 +0700

    Send api get method complete

---
+++ API/Controllers/ClientsController.cs
@@ -0,0 +1,25 @@
+﻿using API.Models;
+using API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public ClientsController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public List<Client> GetClients()
+        {
+            return context.Clients.OrderByDescending(c => c.Id).ToList();
+        }
+    }
+}
